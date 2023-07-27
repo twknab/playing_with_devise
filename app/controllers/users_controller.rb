6ed5c3e53e63f644f324b69abc453f88c3912fb5
twknab <20636750+twknab@users.noreply.github.com>
2023-07-27 04:56:17 -0700

    frame out controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to root_url, notice: "Thanks for signing up!"
+    else
+      render "new"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,5 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :first_name, :last_name, :email, presence: true
+  validates_uniqueness_of :email
 end
