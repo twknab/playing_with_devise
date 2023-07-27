7a9b1ed5836db105748d7b14387776c65c754be7
twknab <20636750+twknab@users.noreply.github.com>
2023-07-27 14:32:37 -0700

    add login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to dashboard_index, notice: "Thanks for signing up!"
+      redirect_to dashboard_index_path, notice: "Thanks for signing up!"
     else
       render :new, status: :unprocessable_entity
     end
