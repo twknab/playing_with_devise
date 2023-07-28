093097ac71c9a859994f3d6885a743e7abbeaea0
twknab <20636750+twknab@users.noreply.github.com>
2023-07-28 05:30:12 -0700

    simplify and fix bug

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,20 +6,19 @@ class DashboardController < ApplicationController
   before_action :authenticate_user
 
   def index
+    user = User.find(current_user.id)
+    @threshold = user.aqi_threshold
+    if user.city && user.aqi_threshold
+      response = fetch_aqi_data(user.city, user.aqi_threshold)
+      if response['status'] == 'ok'
+        @aqi = response['data']['aqi']
+      end
+    end
   end
 
-  # TODO: Break this method up, since it's really doing 2 thigns: fetching the weather AND updating
   def update
-    # @threshold = params[:aqi_treshold]
-    response = fetch_aqi_data(params[:city], params[:aqi_threshold])
-    if response['status'] == 'ok'
-      current_user.update(city: params[:city], aqi_threshold: params[:aqi_threshold])
-      # TODO: Send back response.data.aqi accessible on template side
-      @aqi = response['data']['aqi']
-    else
-      flash[:alert] = "Location not found, please try another City"
-    end
-    render :index
+    current_user.update(city: params[:city], aqi_threshold: params[:aqi_threshold])
+    redirect_to dashboard_path
   end
   # TODO: Add rescue in case the update fails for some reason, handle error gracefully for user and log?
 
