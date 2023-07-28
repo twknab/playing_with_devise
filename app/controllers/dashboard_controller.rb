60a2c7c718f0c4c51473500e727fa5bc97060114
twknab <20636750+twknab@users.noreply.github.com>
2023-07-27 19:00:19 -0700

    add before_action :authenticate_user to dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,14 @@
 class DashboardController < ApplicationController
+  before_action :authenticate_user
+
   def index
+  end
+
+  private
+
+  def authenticate_user
+    unless current_user
+      redirect_to root_path
+    end
   end
 end
